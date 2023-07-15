921ba11178dd32461abaa285c38605eb91b65f9e
meyadavajeet <meyadavajeet@gmail.com>
2023-07-15 19:28:56 +0530

    update task information

--- src/app/api/tasks/[taskId]/route.js
+++ src/app/api/tasks/[taskId]/route.js
@@ -1,6 +1,9 @@
+import { connectDB } from "@/config/db.connection";
 import TaskModel from "@/models/task.model";
 import { sendResponse } from "@/utils/sendResponse";
+import moment from "moment";
 
+connectDB();
 // get task by id
 export async function GET(request, { params }) {
   try {
@@ -22,5 +25,34 @@ export async function DELETE(request, { params }) {
   } catch (error) {
     console.log("error in deleting task", error);
     return sendResponse(false, "", "Error while deleting data!!", 500);
+  }
+}
+
+//update task
+export async function PUT(request, { params }) {
+  try {
+    const { taskId } = params;
+    const { title, content, deadlineDate, addedDate, status } =
+      await request.json();
+    let task = await TaskModel.findById({ _id: taskId });
+    console.log("task available in db", task);
+    task.title = title;
+    task.content = content;
+    task.status = status;
+    task.addedDate = addedDate && moment(addedDate, "YYYY-MM-DD").toDate();
+    task.deadlineDate =
+      deadlineDate && moment(deadlineDate, "YYYY-MM-DD").toDate();
+
+    console.log(task);
+    const updatedTask = await task.save();
+    return sendResponse(true, "Task Updated Successfully!!!", updatedTask);
+  } catch (error) {
+    console.log(error);
+    return sendResponse(
+      false,
+      "",
+      `Error while updating task ${error.message}`,
+      500
+    );
   }
 }
