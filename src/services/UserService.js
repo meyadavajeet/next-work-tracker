9084ea17f32aa994ce66c3330cfc9df5eac39690
meyadavajeet <meyadavajeet@gmail.com>
2023-09-27 23:40:07 +0530

    remove unused code

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,36 +1,30 @@
 import { httpAxios } from "@/helper/HttpHelper";
 
+// export const registerUser = async (user) => {
+//   const result = await httpAxios.post("api/users", user).then((response) => {
+//     response.data;
+//   });
+//   return result;
+// };
+
+// export const loginUser = async (user) => {
+//   const result = await httpAxios.post("api/login", user).then((response) => {
+//     response.data;
+//   });
+//   return result;
+// };
+
 export const registerUser = async (user) => {
-  const result = await httpAxios.post("api/users", user).then((response) => {
-    response.data;
-  });
+  const result = await httpAxios.post("api/users", user);
   return result;
 };
 
 export const loginUser = async (user) => {
-  const result = await httpAxios.post("api/login", user).then((response) => {
-    response.data;
-  });
+  const result = await httpAxios.post("api/login", user);
   return result;
-};
-
-// export const currentUser = async () => {
-//   try {
-//     const response = await httpAxios.get("api/current");
-//     return response.data;
-//   } catch (error) {
-//     // Handle errors here
-//     console.error("Error in currentUser:", error);
-//     throw error; // Re-throw the error if needed
-//   }
-// };
-
-export const currentUser = async () => {
-  const result = await httpAxios.get("api/current");
-  return result.data;
 };
 
 export const logoutUser = async () => {
   const result = await httpAxios.post("api/logout");
-  return result.data;
+  return result;
 };
